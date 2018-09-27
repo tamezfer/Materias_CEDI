031c0c63b58c6bfd51977e16daa8148e07dbcc55
alumno Preparatoria <alprepa@cedi.local>
2018-09-27 08:36:49 -0500

    Se genero una pagina para historia con click

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HistoriaPage } from '../historia/historia';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  historia = HistoriaPage;
   constructor(public navCtrl: NavController) {
 
+  }
+  clickHistoria()
+  {
+    this.navCtrl.push(this.historia);
   }
 
 }
