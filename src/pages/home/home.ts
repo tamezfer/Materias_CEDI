f2ded590db2c2bba45a3e902a14de3e3d8111664
alumno Preparatoria <alprepa@cedi.local>
2018-10-01 08:36:53 -0500

    Generamos una página con todos los elementos que teniamos en la página de historia pero esta vez para dibujo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HistoriaPage } from '../historia/historia';
+import { DibujoPage } from '../dibujo/dibujo';
 
 @Component({
   selector: 'page-home',
@@ -8,12 +9,18 @@ import { HistoriaPage } from '../historia/historia';
 })
 export class HomePage {
   historia = HistoriaPage;
+  dibujo = DibujoPage;
   constructor(public navCtrl: NavController) {
 
   }
   clickHistoria()
   {
     this.navCtrl.push(this.historia);
+  }
+
+  clickDibujo()
+  {
+    this.navCtrl.push(this.dibujo)
   }
 
 }
